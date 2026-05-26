32d9c2717d0440f31cb2901c88e3bfeca72f9c47
raaddohs-droid <raaddohs-droid@gmail.com>
2026-05-26 12:51:24 +0600

    Remove broken test file

--- app/test-photo/page.tsx
+++
@@ -1,9 +0,0 @@
-import AIPhotoCropper from "@/components/profiles/AIPhotoCropper";
-
-export default function TestPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <AIPhotoCropper />
-    </div>
-  );
-}
